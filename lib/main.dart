767d3baf226320b16ee2432f7aca1a8fe831ee7d
Renato <renatoflores2012@gmail.com>
2022-07-07 18:15:59 -0500

    add news animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animation/screens/align_animation.dart';
+import 'package:flutter_animation/screens/animated_builder.dart';
+import 'package:flutter_animation/screens/animated_container.dart';
+import 'package:flutter_animation/screens/animated_cross_fade.dart';
+import 'package:flutter_animation/screens/animated_default_text_style.dart';
+import 'package:flutter_animation/screens/animated_list_state.dart';
+import 'package:flutter_animation/screens/animated_modal_barrier.dart';
+import 'package:flutter_animation/screens/animated_opacity.dart';
+import 'package:flutter_animation/screens/animated_physical_model.dart';
+import 'package:flutter_animation/screens/animated_positioned.dart';
+import 'package:flutter_animation/screens/animated_size.dart';
+import 'package:flutter_animation/screens/animated_widget.dart';
 
 void main() => runApp(const MyApp());
 
@@ -25,25 +36,59 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        centerTitle: true,
-        title: const Text('Animations'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(8.0),
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: (() {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const AlignScreen()));
-              }),
-              child: const Text('AlignAnimation'),
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          centerTitle: true,
+          title: const Text('Animations'),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(8.0),
+            width: double.infinity,
+            height: double.infinity,
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    button(context, 'AlignAnimation', const AlignScreen()),
+                    button(context, 'AnimatedBuilder',
+                        const AnimatedBuilderScreen()),
+                    button(context, 'AnimatedContainer',
+                        const AnimatedContainerScreen()),
+                    button(context, 'AnimatedCrossFade',
+                        const AnimatedCrossFadeScreen()),
+                    button(context, 'AnimatedDefaultTextStyle',
+                        const AnimatedDefaultTextStyleScreen()),
+                    button(context, 'AnimatedListState',
+                        const AnimatedListStateScreen()),
+                    button(context, 'AnimatedModalBarrier',
+                        const AnimatedModalBarrierScreen()),
+                    button(context, 'AnimatedOpacity',
+                        const AnimatedOpacityScreen()),
+                    button(context, 'AnimatedPshysicalModel',
+                        const AnimatedPhysicalModelScreen()),
+                    button(context, 'AnimatedPositioned',
+                        const AnimatedPositionedScreen()),
+                    button(context, 'AnimatedSize', const AnimatedSizeScreen()),
+                    button(context, 'AnimatedWidget',
+                        const AnimatedWidgetScreen()),
+                  ]),
+            )));
+  }
+
+  Widget button(context, String text, route) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        constraints: const BoxConstraints(
+          minWidth: 180,
+          minHeight: 30,
+        ),
+        child: ElevatedButton(
+          onPressed: (() {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => route));
+          }),
+          child: Text(text),
         ),
       ),
     );
